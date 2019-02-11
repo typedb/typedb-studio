701efde3a7409e631d38d3583c7705f41a6b7c08
Max Vorobev <vmax0770@gmail.com>
2019-02-11 15:30:55 +0300

    fix from review: document steps in instance-setup-template.ps1

--- .circleci/build-workbase-win/instance-setup-template.ps1
+++ .circleci/build-workbase-win/instance-setup-template.ps1
@@ -1,8 +1,12 @@
+# Enabling OpenSSH
 Add-WindowsCapability -Online -Name OpenSSH.Server~~~~0.0.1.0
 Start-Service sshd
 Set-Service -Name sshd -StartupType 'Automatic'
+# Create a new admin user and allow him to use elevated priveleges
 New-LocalUser -Name circleci -Password $(ConvertTo-SecureString "INSTANCE_PASSWORD" -AsPlainText -Force)
 Add-LocalGroupMember -Group "Administrators" -Member "circleci"
 # https://github.com/PowerShell/Win32-OpenSSH/issues/962
 reg add HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\system /v LocalAccountTokenFilterPolicy /t REG_DWORD /d 1 /f
+
+# Download and install Chocolatey, which is a package manager for Windows
 Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
