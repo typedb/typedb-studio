724af3d9f2d6de579215ee8efb18e957bf73a108
Max Vorobev <vmax0770@gmail.com>
2019-08-27 13:00:36 +0300

    Use yarn in CI (#189)
    ## What is the goal of this PR?
    
    Fix #172
    Adapt `workbase` CI pipeline to latest `rules-nodejs`
    
    ## What are the changes implemented in this PR?
    
    * (behind-the-scenes) Upgraded GCP image `circleci-workbase-build` to use `bazel 0.26.0` and contain `wheel` Python dependency (was previously installed manually as CI step)
    * Use `yarn` in place of `npm` everywhere (`npm` cannot be used due to bazelbuild/rules_nodejs#948)
    * Refactor `install-node-electron.sh` to properly download the binary (`yarn` does not recognize `--runtime=electron --target=3.0.6` that were previously used with `npm`)

--- install-node-electron.sh
+++ install-node-electron.sh
@@ -6,7 +6,24 @@ orig_dir=$(pwd)
 temp_dir=$(mktemp -d)
 
 pushd ${temp_dir}
-${npm_node_execpath} ${npm_execpath} install grpc --runtime=electron --target=3.0.6
-cp -Rv ./node_modules/grpc/src/node/extension_binary/electron-* ${orig_dir}/node_modules/grpc/src/node/extension_binary/
+LINUX_URL="https://node-precompiled-binaries.grpc.io/grpc/v1.23.2/electron-v3.0-linux-x64-glibc.tar.gz"
+DARWIN_URL="https://node-precompiled-binaries.grpc.io/grpc/v1.23.2/electron-v3.0-darwin-x64-unknown.tar.gz"
+WINDOWS_URL="https://node-precompiled-binaries.grpc.io/grpc/v1.23.3/electron-v3.0-win32-x64-unknown.tar.gz"
+
+platform=$(uname)
+if [[ "$platform" == "Darwin" ]]; then
+    DOWNLOAD_URL="$DARWIN_URL"
+elif [[ "$platform" == "Linux" ]]; then
+    DOWNLOAD_URL="$LINUX_URL"
+elif [[ "$platform" == "MSYS_NT-10.0" ]]; then
+    DOWNLOAD_URL="$WINDOWS_URL"
+else
+  echo "grpc does not have a binary for $platform"
+  exit 1
+fi
+
+curl -LO $DOWNLOAD_URL
+tar xvf *.tar.gz && rm *.tar.gz
+cp -Rv electron-* ${orig_dir}/node_modules/grpc/src/node/extension_binary/
 popd
 rm -rf ${temp_dir}
