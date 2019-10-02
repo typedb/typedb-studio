a351a1227d2827ae6aaa61060f957b4d630e0bc7
Max Vorobev <vmax0770@gmail.com>
2019-10-02 19:29:30 +0300

    Migrate assemble-windows to CircleCI Windows

---
+++ .circleci/windows/assemble.bat
@@ -0,0 +1,57 @@
+@echo off
+REM
+REM GRAKN.AI - THE KNOWLEDGE GRAPH
+REM Copyright (C) 2019 Grakn Labs Ltd
+REM
+REM This program is free software: you can redistribute it and/or modify
+REM it under the terms of the GNU Affero General Public License as
+REM published by the Free Software Foundation, either version 3 of the
+REM License, or (at your option) any later version.
+REM
+REM This program is distributed in the hope that it will be useful,
+REM but WITHOUT ANY WARRANTY; without even the implied warranty of
+REM MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+REM GNU Affero General Public License for more details.
+REM
+REM You should have received a copy of the GNU Affero General Public License
+REM along with this program.  If not, see <https://www.gnu.org/licenses/>.
+REM
+
+REM needs to be called such that software installed
+REM by Chocolatey in prepare.bat is accessible
+CALL refreshenv
+
+REM build grakn-core-all-windows archive
+bazel build @graknlabs_grakn_core//:assemble-windows-zip || GOTO :error
+
+REM unpack and start Grakn server
+unzip bazel-genfiles\external\graknlabs_grakn_core\grakn-core-all-windows.zip -d bazel-genfiles\dist\ || GOTO :error
+PUSHD bazel-genfiles\dist\grakn-core-all-windows\
+CALL grakn.bat server start || GOTO :error
+CALL grakn.bat console -f ..\..\..\test\helpers\gene.gql -k gene || GOTO :error
+POPD
+
+REM run unit tests
+bazel run @nodejs//:bin/yarn.cmd -- run unit || GOTO :error
+
+REM run integration tests
+bazel run @nodejs//:bin/yarn.cmd -- run integration || GOTO :error
+
+REM stop Grakn server
+PUSHD bazel-genfiles\dist\grakn-core-all-windows\
+CALL grakn.bat server stop
+POPD
+
+REM build Grakn Workbase
+bazel run @nodejs//:bin/yarn.cmd -- run build || GOTO :error
+
+REM store Grakn Workbase for subsequent deployment
+MKDIR distribution
+set /p version=<VERSION
+MOVE .\build\GRAKNW~1.EXE distribution\grakn-workbase-windows-%version%.exe || GOTO :error
+
+REM verify that Grakn Workbase executable file was successfully copied
+DIR distribution\
+
+:error
+IF %errorlevel% NEQ 0 EXIT /b %errorlevel%
