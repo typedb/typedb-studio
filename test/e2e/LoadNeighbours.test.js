0f14f57e44ffdb27e7bc7e4de83ea7dc3551f0a1
Soroush Saffari <soroush@grakn.ai>
2019-10-23 12:55:39 +0100

    uncomment a test in e2e/LoadNeighbours.test.js and improve assertions

--- test/e2e/LoadNeighbours.test.js
+++ test/e2e/LoadNeighbours.test.js
@@ -1,6 +1,6 @@
 import assert from 'assert';
 import { startApp, stopApp } from './helpers/hooks';
-import { selectKeyspace, waitForQueryCompletion, runQuery, doubleClickOnNode } from './helpers/actions';
+import { selectKeyspace, waitForQueryCompletion, runQuery, doubleClickOnNode, changeSetting } from './helpers/actions';
 import { getNodePosition } from './helpers/canvas';
 import { loadKeyspace, deleteKeyspace } from './helpers/utils';
 
@@ -30,56 +30,53 @@ afterAll(async () => {
 describe('Load Neighbours', () => {
   test('double clicking on an entity loads the relations it plays a role in', async () => {
     await selectKeyspace('gene', app);
+    await changeSetting({ neighboursLimit: 1 }, app);
     await runQuery('match $x isa person; get; offset 0; limit 1;', app);
     const targetNodeCoordinates = await getNodePosition({ by: 'var', value: 'x' }, app);
     await doubleClickOnNode(targetNodeCoordinates, app);
     await waitForQueryCompletion(app);
-    const numOfEntities = Number((await app.client.getText('.no-of-entities')).match(/\d+/)[0]);
-    assert.equal(numOfEntities > 1, true);
-    const noOfRelationships = Number((await app.client.getText('.no-of-relations')).match(/\d+/)[0]);
-    assert.equal(noOfRelationships > 0, true);
+
+    const noOfRelations = Number((await app.client.getText('.no-of-relations')).match(/\d+/)[0]);
+    assert.equal(noOfRelations, 1);
   });
 
   test('double clicking on an attribute instance loads its owners', async () => {
     await selectKeyspace('gene', app);
+    await changeSetting({ neighboursLimit: 1 }, app);
     await runQuery('match $x isa age; get; offset 0; limit 1;', app);
     const targetNodeCoordinates = await getNodePosition({ by: 'var', value: 'x' }, app);
     await doubleClickOnNode(targetNodeCoordinates, app);
     await waitForQueryCompletion(app);
+
     const noOfEntities = Number((await app.client.getText('.no-of-entities')).match(/\d+/)[0]);
-    assert.equal(noOfEntities > 0, true);
-    const noOfRelationships = await app.client.getText('.no-of-relations');
-    assert.equal(noOfRelationships, 'relations: 0');
+    const noOfRelations = Number((await app.client.getText('.no-of-relations')).match(/\d+/)[0]);
+    const noOfAttributes = Number((await app.client.getText('.no-of-attributes')).match(/\d+/)[0]);
+    assert.equal(noOfEntities + noOfRelations + noOfAttributes, 2);
   });
 
-  // test('double clicking on a relation instance loads its roleplayers', async () => {
-  //   // TODO:  turn off load role players here
-  //   await app.client.click('.top-bar-container .CodeMirror');
-  //   await app.client.keys('match $x isa parentship; get; offset 0; limit 1;');
-  //   await app.client.click('.run-btn');
-  //   await waitForQueryCompletion(app);
+  test('double clicking on a relation instance loads its roleplayers', async () => {
+    await selectKeyspace('gene', app);
+    await changeSetting({ loadRoleplayers: false, neighboursLimit: 1 }, app);
+    await runQuery('match $x isa parentship; get; offset 0; limit 1;', app);
+    const targetNodeCoordinates = await getNodePosition({ by: 'var', value: 'x' }, app);
+    await doubleClickOnNode(targetNodeCoordinates, app);
+    await waitForQueryCompletion(app);
 
-  //   const targetNodeId = await getNodeId({ by: 'var', value: 'x' }, app);
-  //   await waitForNodeToStabalize(targetNodeId, app);
-  //   const targetNodeCoordinates = await getNodePosition(targetNodeId, app);
-  //   await app.client.rightClick('#graph-div', targetNodeCoordinates.x, targetNodeCoordinates.y);
-  //   await doubleClick('#graph-div', targetNodeCoordinates, app);
-  //   await waitForQueryCompletion(app);
-
-  //   const noOfEntities = Number((await app.client.getText('.no-of-entities')).match(/\d+/)[0]);
-  //   expect(noOfEntities).toBeGreaterThan(0);
-
-  //   const noOfRelationships = Number((await app.client.getText('.no-of-relations')).match(/\d+/)[0]);
-  //   expect(noOfRelationships).toBeGreaterThan(0);
-  // });
+    const noOfEntities = Number((await app.client.getText('.no-of-entities')).match(/\d+/)[0]);
+    const noOfRelations = Number((await app.client.getText('.no-of-relations')).match(/\d+/)[0]);
+    const noOfAttributes = Number((await app.client.getText('.no-of-attributes')).match(/\d+/)[0]);
+    assert.equal(noOfEntities + noOfRelations + noOfAttributes, 2);
+  });
 
   test('double clicking on a type loads its instances', async () => {
     await selectKeyspace('gene', app);
+    await changeSetting({ neighboursLimit: 1 }, app);
     await runQuery('match $x sub person; get; offset 0; limit 1;', app);
     const targetNodeCoordinates = await getNodePosition({ by: 'var', value: 'x' }, app);
     await doubleClickOnNode(targetNodeCoordinates, app);
     await waitForQueryCompletion(app);
+
     const noOfEntities = Number((await app.client.getText('.no-of-entities')).match(/\d+/)[0]);
-    assert.equal(noOfEntities > 0, true);
+    assert.equal(noOfEntities, 1);
   });
 });
