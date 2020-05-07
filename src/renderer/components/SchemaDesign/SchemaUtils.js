306a688e895c28da58bbfe0154b9a5bbc71aaece
Soroush Saffari <tools@soroushsaffari.com>
2020-05-07 16:30:49 +0100

    misc

--- src/renderer/components/SchemaDesign/SchemaUtils.js
+++ src/renderer/components/SchemaDesign/SchemaUtils.js
@@ -44,13 +44,12 @@ export async function loadMetaTypeInstances(graknTx) {
 
 // attach attribute labels and data types to each node
 export async function computeAttributes(nodes, graknTx) {
-  const nodesComp = await Promise.all(nodes.map(async (node) => {
+  return Promise.all(nodes.map(async (node) => {
     const concept = await graknTx.getSchemaConcept(node.label);
     const attributes = await (await concept.attributes()).collect();
     node.attributes = await Promise.all(attributes.map(async concept => ({ type: await concept.label(), dataType: await concept.dataType() })));
     return node;
   }));
-  return nodesComp;
 }
 
 // attach role labels to each node
