6b4b068de40e57aaa83e42165aca41e80c744993
Soroush Saffari <soroush@grakn.ai>
2020-06-12 16:03:15 +0100

    Ensures closing tabs works reliably and as expected (#289)
    ## What is the goal of this PR?
    Changes introduced in this PR, ensure that closing a tab always works and it does not reset the state of other tabs.
    
    ## What are the changes implemented in this PR?
    resolves https://github.com/graknlabs/workbase/issues/176
    resolves https://github.com/graknlabs/workbase/issues/288
    
    ### `VisualiserContent.vue`: Use and Object instead of a Map for `tabs`
    Maps are not reactive in Vue (yet).
    Objects have a simpler syntax compared to Maps, which provide better readability
    
    ### VisualiserContent.vue`: Remove extra logic from `closeTab`
    The following is weird (!) and fortunately no longer needed
    ```
    // re-set the same tab to trigger dynamic rerendering of list
    const temp = this.currentTab;	
    this.currentTab = null;	
    this.currentTab = temp;
    ```
    
    ### Add e2e tests for interacting with Tabs

--- src/renderer/components/Visualiser/VisualiserContent.vue
+++ src/renderer/components/Visualiser/VisualiserContent.vue
@@ -1,32 +1,31 @@
 <template>
     <transition name="fade" appear>
         <div>
-
             <div class="vis-tabs noselect">
-              <div v-for="tab in Array.from(tabs.keys())" :key="tab">
-                <div :class="(tab === currentTab) ? 'tab current-tab' : 'tab'">
+              <div v-for="(tabTitle, tab) in tabs" :key="tab">
+                <div :id="`tab-${tab}`" :class="(tab === currentTab) ? 'tab current-tab' : 'tab'">
+                  <div class="tab-content">
+                    <template v-if="tabToRename !== tab">
+                      <div @click="toggleTab(tab)" @dblclick="renameTab(tab)" class="tab-title">{{ tabTitle || `Tab ${tab}` }}</div>
+                      <div v-if="Object.keys(tabs).length > 1" @click="closeTab(tab)" class="close-tab-btn"><vue-icon className="tab-icon" icon="cross" iconSize="13"></vue-icon></div>
+                    </template>
 
-                  <div class="tab-content" v-if="tabToRename !== tab">
-                    <div @click="toggleTab(tab)" @dblclick="renameTab(tab)" class="tab-title">{{(tabs.get(tab)) ? tabs.get(tab) : `Tab ${tab}`}}</div>
-                    <div v-if="tabs.size > 1" @click="closeTab(tab)" class="close-tab-btn"><vue-icon className="tab-icon" icon="cross" iconSize="13"></vue-icon></div>
+                    <template v-else>
+                      <input ref="renameTabInput" class="input-small rename-tab-input" v-model="newTabName">
+                      <div @click="cancelRename" class="cancel-tab-rename-btn"><vue-icon className="tab-icon" icon="cross" iconSize="13"></vue-icon></div>
+                      <div  @click="saveName(tab)" class="save-tab-rename-btn"><vue-icon className="tab-icon" icon="tick" iconSize="13"></vue-icon></div>
+                    </template>
                   </div>
-                  
-                  <div v-else class="tab-content">
-                    <input ref="renameTabInput" class="input-small rename-tab-input" v-model="newTabName">
-                    <div @click="cancelRename" class="close-tab-btn"><vue-icon className="tab-icon" icon="cross" iconSize="13"></vue-icon></div>
-                    <div  @click="saveName(tab)" class="close-tab-btn"><vue-icon className="tab-icon" icon="tick" iconSize="13"></vue-icon></div>
-                  </div>
-
                 </div>
               </div>
-              <button v-if="tabs.size < 10" @click="newTab" class='btn new-tab-btn'><vue-icon icon="plus" className="vue-icon"></vue-icon></button>
+              <button v-if="Object.keys(tabs).length < 10" @click="newTab" class='btn new-tab-btn'><vue-icon icon="plus" className="vue-icon"></vue-icon></button>
             </div>
 
-              <template v-for="tab in Array.from(tabs.keys())">
-                <keep-alive :key="tab">
+              <div v-for="tab in Object.keys(tabs)" :key="tab">
+                <keep-alive>
                   <component v-if="currentTab === tab" :is="visTab" :tabId="tab" :key="tab"></component>
                 </keep-alive>
-              </template>
+              </div>
 
         </div>
     </transition>
@@ -96,8 +95,8 @@ export default {
   components: { VisTab },
   data() {
     return {
-      currentTab: 1,
-      tabs: new Map([[1, undefined]]),
+      currentTab: '1',
+      tabs: { 1: undefined },
       visTab: 'VisTab',
       LETTER_T_KEYCODE: 84,
       tabToRename: undefined,
@@ -115,7 +114,7 @@ export default {
   created() {
     window.addEventListener('keydown', (e) => {
       // pressing CMD + T will create a new tab
-      if ((e.keyCode === this.LETTER_T_KEYCODE) && e.metaKey && this.tabs.size < 10) this.newTab();
+      if ((e.keyCode === this.LETTER_T_KEYCODE) && e.metaKey && Object.keys(this.tabs).length < 10) this.newTab();
 
       if (this.tabToRename && e.keyCode === 13) this.saveName(this.tabToRename); // Pressing enter will save tab name
       if (this.tabToRename && e.keyCode === 27) this.tabToRename = undefined; // Pressing escape will cancel renaming of tab
@@ -134,34 +133,31 @@ export default {
       this.setActiveTab(`tab-${tab}`);
     },
     newTab() {
-      const newTabId = Math.max(...Array.from(this.tabs.keys())) + 1; // Get max tab id and increment it for new tab id
-      this.tabs.set(newTabId, undefined);
+      const newTabId = `${Math.max(...Object.keys(this.tabs)) + 1}`; // Get max tab id and increment it for new tab id
+      this.tabs[newTabId] = undefined;
       this.currentTab = newTabId;
       this.setActiveTab(`tab-${newTabId}`);
     },
     async closeTab(tab) {
-      // Find tab compoenent which has same tabId as tab to be closed and destroy it manually
-      const visTab = this.$children.find(x => (x.tabId && x.tabId === tab));
-      visTab.$destroy();
+      const visTabToClose = this.$children.find(x => x.tabId && x.tabId === tab);
+      visTabToClose.$destroy();
 
-      this.tabs.delete(tab);
+      // vue doesn't detect property deleteion, so we clone, delete and reassign
+      const tabsClone = { ...this.tabs };
+      delete tabsClone[tab];
+      this.tabs = tabsClone;
 
-      // if tab being closes is same as current tab switch to first tab in tabs
-      if (this.currentTab === tab) this.currentTab = Array.from(this.tabs.keys())[0];
-      else { // re-set the same tab to trigger dynamic rerendering of list
-        const temp = this.currentTab;
-        this.currentTab = null;
-        this.currentTab = temp;
-      }
+      if (this.currentTab === tab) this.currentTab = Object.keys(this.tabs)[0];
+
       this.setActiveTab(`tab-${this.currentTab}`);
     },
     renameTab(tab) {
       this.tabToRename = tab;
-      this.newTabName = this.tabs.get(tab);
+      this.newTabName = this.tabs[tab];
       this.$nextTick(() => this.$refs.renameTabInput[0].focus());
     },
     saveName(tab) {
-      this.tabs.set(tab, this.truncate(this.newTabName));
+      this.tabs[tab] = this.truncate(this.newTabName);
       this.cancelRename();
     },
     cancelRename() {
