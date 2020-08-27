38b7f1d25fed9b23854a363a162ac20b065f5728
Soroush Saffari <soroush@grakn.ai>
2020-08-27 20:47:26 +0100

    refreshenv after choco install in prepare.bat

--- .circleci/windows/prepare.bat
+++ .circleci/windows/prepare.bat
@@ -22,6 +22,7 @@ choco uninstall openjdk --limit-output --yes --no-progress
 
 REM install dependencies needed for build
 choco install .circleci\windows\dependencies.config  --limit-output --yes --no-progress
+refreshenv
 
 REM create a symlink python3.exe and make it available in %PATH%
 mklink C:\Python37\python3.exe C:\Python37\python.exe
