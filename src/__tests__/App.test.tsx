663762815bac3a35ae4ba3eed9114b11e319a15d
Alex Walker <alex@grakn.ai>
2021-07-07 12:55:46 +0100

    Delete unwanted boilerplate code

--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import '@testing-library/jest-dom';
 import { render } from '@testing-library/react';
-import App from '../App';
+import { StudioApp } from '../app';
 
-describe('App', () => {
-  it('should render', () => {
-    expect(render(<App />)).toBeTruthy();
-  });
+describe('StudioApp', () => {
+    it('should render', () => {
+        expect(render(<StudioApp/>)).toBeTruthy();
+    });
 });
