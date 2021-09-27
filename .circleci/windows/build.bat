9e8500aa8a2000db1eb4906739ad7904d4a4a337
Alex Walker <alex@grakn.ai>
2021-09-27 17:30:41 +0100

    Fix dist dir not existing on Windows

--- .circleci/windows/build.bat
+++ .circleci/windows/build.bat
@@ -24,9 +24,7 @@ bazel build //:application-image
 IF %errorlevel% NEQ 0 EXIT /b %errorlevel%
 
 ECHO Extracting application image archive...
-mkdir ~/src
-xcopy ../bazel-bin/application-image.zip ~/src
-mkdir ~/dist
-cd ~/dist
-jar xf ~/src/application-image.zip
+mkdir dist
+cd dist
+jar xf ..\bazel-bin\application-image.zip
 IF %errorlevel% NEQ 0 EXIT /b %errorlevel%
