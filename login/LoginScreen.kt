1861f63510392359167c730ebeff5f5fb0735464
Alex Walker <alex@grakn.ai>
2021-10-06 17:38:06 +0100

    Make ENTER button submit login form

--- login/LoginScreen.kt
+++ login/LoginScreen.kt
@@ -1,20 +1,34 @@
 package com.vaticle.typedb.studio.login
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.focusable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.Button
 import androidx.compose.material.SnackbarDuration
 import androidx.compose.material.SnackbarHostState
 import androidx.compose.material.Text
 import androidx.compose.material.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusOrder
+import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.focus.focusTarget
+import androidx.compose.ui.input.key.Key
+import androidx.compose.ui.input.key.KeyEvent
+import androidx.compose.ui.input.key.key
+import androidx.compose.ui.input.key.onKeyEvent
+import androidx.compose.ui.input.key.onPreviewKeyEvent
 import androidx.compose.ui.unit.dp
 import com.vaticle.typedb.studio.appearance.StudioTheme
 import com.vaticle.typedb.studio.data.DB
@@ -24,10 +38,12 @@ import com.vaticle.typedb.studio.navigation.Navigator
 import com.vaticle.typedb.studio.navigation.WorkspaceScreenState
 import kotlinx.coroutines.launch
 
+@OptIn(ExperimentalComposeUiApi::class)
 @Composable
 fun LoginScreen(form: LoginScreenState, navigator: Navigator, snackbarHostState: SnackbarHostState) {
 
     val snackbarCoroutineScope = rememberCoroutineScope()
+    val (addressFocus, dbNameFocus) = listOf(FocusRequester(), FocusRequester())
 
     Box(modifier = Modifier.fillMaxSize().background(StudioTheme.colors.windowBackdrop), contentAlignment = Alignment.Center) {
         Box(modifier = Modifier.size(480.dp, 360.dp).background(StudioTheme.colors.background), contentAlignment = Alignment.Center) {
@@ -45,12 +61,35 @@ fun LoginScreen(form: LoginScreenState, navigator: Navigator, snackbarHostState:
                             snackbarHostState.showSnackbar(e.toString(), actionLabel = "HIDE", SnackbarDuration.Long)
                         }
                     }
-                }, label = { Text("Server address") })
+                }, label = { Text("Server address") }, modifier = Modifier
+                    .focusRequester(addressFocus)
+                    .onPreviewKeyEvent { event: KeyEvent ->
+                        when (event.key) {
+                            Key.Tab -> {
+                                dbNameFocus.requestFocus()
+                                return@onPreviewKeyEvent true
+                            }
+                            else -> return@onPreviewKeyEvent false
+                        }
+                    })
 
                 TextField(value = form.dbName, onValueChange = {
                     form.dbName = it
-                    form.dbClient?.let { dbClient -> form.db = DB(client = dbClient, dbName = form.dbName) }
-                }, label = { Text("Database") })
+                    form.db = DB(form.dbClient, form.dbName)
+                }, label = { Text("Database") }, singleLine = true, modifier = Modifier
+                    .focusRequester(dbNameFocus)
+                    .onPreviewKeyEvent { event: KeyEvent ->
+                        when (event.key) {
+                            Key.Tab -> {
+                                return@onPreviewKeyEvent true
+                            }
+                            Key.Enter -> {
+                                form.db?.let { navigator.pushState(WorkspaceScreenState(db = it)) }
+                                return@onPreviewKeyEvent true
+                            }
+                            else -> return@onPreviewKeyEvent false
+                        }
+                    })
 
                 Button(enabled = form.db != null, onClick = {
                     navigator.pushState(WorkspaceScreenState(db = requireNotNull(form.db)))
@@ -59,5 +98,9 @@ fun LoginScreen(form: LoginScreenState, navigator: Navigator, snackbarHostState:
                 }
             }
         }
+    }
+
+    LaunchedEffect(Unit) {
+        addressFocus.requestFocus()
     }
 }
