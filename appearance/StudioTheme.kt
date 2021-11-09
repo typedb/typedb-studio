ccbc234e3764cee926533c2daf667220e2ba45cc
haikalpribadi <haikal@vaticle.com>
2021-11-09 16:45:40 +0000

    Add license headers to source code

--- appearance/StudioTheme.kt
+++ appearance/StudioTheme.kt
@@ -1,5 +1,23 @@
 // Implementation notes: https://cs.android.com/androidx/platform/frameworks/support/+/androidx-main:compose/material/material/src/commonMain/kotlin/androidx/compose/material/Colors.kt
 
+/*
+ * Copyright (C) 2021 Vaticle
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ */
+
 package com.vaticle.typedb.studio.appearance
 
 import androidx.compose.material.Colors
