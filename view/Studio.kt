1039329cae4c6f7eab2fc5cbe03dc5e733a1b9af
Alex Walker <alex@vaticle.com>
2022-06-17 16:23:54 +0100

    Shrink window icon to 32px

--- view/Studio.kt
+++ view/Studio.kt
@@ -130,7 +130,7 @@ object Studio {
         Window(
             title = getMainWindowTitle(),
             state = rememberWindowState(WindowPlacement.Maximized),
-            icon = painterResource("resources/icons/vaticle/vaticle-bot-64px.png"),
+            icon = painterResource("resources/icons/vaticle/vaticle-bot-32px.png"),
             onPreviewKeyEvent = { handleKeyEvent(it, ::confirmClose) },
             onCloseRequest = { if (error != null) exitApplicationFn() else confirmClose() },
         ) {
