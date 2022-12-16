2cc1e197fc9192999c5759196d9a4825fcec857c
James Williams <james.williams@vaticle.com>
2022-12-16 13:01:26 +0000

    feat: return@ is simpler

--- module/preference/PreferenceDialog.kt
+++ module/preference/PreferenceDialog.kt
@@ -441,13 +441,8 @@ object PreferenceDialog {
                 invalidWarning = Label.PREFERENCE_TRANSACTION_TIMEOUT_MINS_INPUT_WARNING,
                 caption = PREFERENCES_TRANSACTION_TIMEOUT_CAPTION
             ) {/* validator = */
-                val transactionTimeoutMins = it.toLongOrNull()
-
-                if (transactionTimeoutMins != null) {
-                    transactionTimeoutMins in 0..10000
-                } else {
-                    false
-                }
+                val transactionTimeoutMins = it.toLongOrNull() ?: return@TextInputValidated false
+                transactionTimeoutMins in 0..10000
             }
 
             override val preferences: List<PreferenceField> = listOf(matchQueryLimit, transactionTimeoutMins)
