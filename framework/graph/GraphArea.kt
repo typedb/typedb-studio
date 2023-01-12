66ffd8e7a311de8d4c977fd1b02b2f06a81f2068
James Williams <james.williams@vaticle.com>
2023-01-12 11:59:18 +0000

    fix: typo

--- framework/graph/GraphArea.kt
+++ framework/graph/GraphArea.kt
@@ -271,9 +271,7 @@ class GraphArea(transactionState: TransactionState) {
                         onDoubleTap = { point ->
                             graphArea.viewport.findVertexAt(point, graphArea.interactions)?.let {
                                 // TODO: this should require SHIFT-doubleclick, not doubleclick
-                                if (it is Vertex.Thing && it.thing.isInferred) {
-                                    graphArea.graphState.explain(it)
-                                }
+                                if (it is Vertex.Thing && it.thing.isInferred) graphArea.graphState.explain(it)
                             }
                         }
                     ) /* onTap = */ { point ->
