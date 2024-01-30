63ae2d77f7bafce799af27455672c30f44d6060d
Dmitrii Ubskii <18616863+dmitrii-ubskii@users.noreply.github.com>
2024-01-30 16:35:45 +0000

    update brew template

--- config/brew/typedb-studio.rb
+++ config/brew/typedb-studio.rb
@@ -19,12 +19,12 @@ cask 'typedb-studio' do
   version '{version}'
 
   on_arm do
-    url "https://github.com/vaticle/typedb-studio/releases/download/{version}/typedb-studio-mac-arm64-{version}.dmg"
+    url "https://repo.typedb.com/public/public-release/raw/names/typedb-studio-mac-arm64/versions/{version}/typedb-studio-windows-arm64-{version}.zip"
     sha256 "{sha256-arm64}"
   end
 
   on_intel do
-    url "https://github.com/vaticle/typedb-studio/releases/download/{version}/typedb-studio-mac-x86_64-{version}.dmg"
+    url "https://repo.typedb.com/public/public-release/raw/names/typedb-studio-mac-x86_64/versions/{version}/typedb-studio-windows-x86_64-{version}.zip"
     sha256 "{sha256-x86_64}"
   end
 
