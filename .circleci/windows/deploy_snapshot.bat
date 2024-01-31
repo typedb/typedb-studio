026e54727d4abd1a1c8112a5b7e3c9db3d76e888
Dmitrii Ubskii <18616863+dmitrii-ubskii@users.noreply.github.com>
2024-01-31 16:46:42 +0000

    add snapshot versioning hack

--- .circleci/windows/deploy_snapshot.bat
+++ .circleci/windows/deploy_snapshot.bat
@@ -20,5 +20,12 @@ REM by Chocolatey in prepare.bat is accessible
 CALL refreshenv
 
 ECHO Building Windows application image...
-bazel --output_user_root=C:/b --define version=%CIRCLE_SHA1% run //:deploy-windows-x86_64-exe --compilation_mode=opt -- snapshot
+
+REM TODO Temporary measure. This exists for two reasons:
+REM 1) platform-jvm assembly rules currently requires the version to be specified in a version_file
+REM 2) jpackage does not support 0 as a major version
+REM This writes VERSION-SHA1 into the VERSION file to be used by the assembly rule.
+FOR /F "tokens=*" %%V IN (VERSION) DO (SET VERS=%%V) & ECHO %VERS%-%CIRCLE_SHA%> VERSION
+
+bazel --output_user_root=C:/b run //:deploy-windows-x86_64-exe --compilation_mode=opt -- snapshot
 IF %errorlevel% NEQ 0 EXIT /b %errorlevel%
