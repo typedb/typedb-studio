d0844ca011005c0e0f18dc149f1f1e0cbf564811
Dmitrii Ubskii <18616863+dmitrii-ubskii@users.noreply.github.com>
2024-01-31 13:01:37 +0000

    fix brew links

--- config/brew/typedb-studio.rb
+++ config/brew/typedb-studio.rb
@@ -19,12 +19,12 @@ cask 'typedb-studio' do
   version '{version}'
 
   on_arm do
-    url "https://repo.typedb.com/public/public-release/raw/names/typedb-studio-mac-arm64/versions/{version}/typedb-studio-windows-arm64-{version}.zip"
+    url "https://repo.typedb.com/public/public-release/raw/names/typedb-studio-mac-arm64/versions/{version}/typedb-studio-mac-arm64-{version}.dmg"
     sha256 "{sha256-arm64}"
   end
 
   on_intel do
-    url "https://repo.typedb.com/public/public-release/raw/names/typedb-studio-mac-x86_64/versions/{version}/typedb-studio-windows-x86_64-{version}.zip"
+    url "https://repo.typedb.com/public/public-release/raw/names/typedb-studio-mac-x86_64/versions/{version}/typedb-studio-mac-x86_64-{version}.dmg"
     sha256 "{sha256-x86_64}"
   end
 
