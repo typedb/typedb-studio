1613688f0bbfed4b8b435d26a40318d33faeaaa7
Haikal Pribadi <haikal@vaticle.com>
2024-11-05 11:58:42 +0000

    Update prepare.bat to install BAZEL_VC and Python 3.11

--- .circleci/windows/prepare.bat
+++ .circleci/windows/prepare.bat
@@ -5,6 +5,7 @@ REM file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
 REM shorten the workspace name so that we can avoid the long path restriction
 git apply .circleci\windows\short_workspace.patch
+IF %ERRORLEVEL% NEQ 0 EXIT /B %ERRORLEVEL%
 
 REM uninstall Java 12 installed by CircleCI
 choco uninstall openjdk --limit-output --yes --no-progress
@@ -13,12 +14,13 @@ REM install dependencies needed for build
 choco install .circleci\windows\dependencies.config  --limit-output --yes --no-progress
 
 REM create a symlink python3.exe and make it available in %PATH%
-mklink C:\Python39\python3.exe C:\Python39\python.exe
-SET PATH=%PATH%;C:\Python39
+mklink C:\Python311\python3.exe C:\Python311\python.exe
+set PATH=%PATH%;C:\Python311
 
 REM install runtime dependency for the build
-C:\Python39\python.exe -m pip install wheel
+C:\Python311\python.exe -m pip install wheel
 
 REM permanently set variables for Bazel build
 SETX BAZEL_SH "C:\Program Files\Git\usr\bin\bash.exe"
-SETX BAZEL_PYTHON C:\Python39\python.exe
+SETX BAZEL_PYTHON C:\Python311\python.exe
+SETX BAZEL_VC "C:\Program Files (x86)\Microsoft Visual Studio\2022\BuildTools\VC"
