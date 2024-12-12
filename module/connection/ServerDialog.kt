6e04f4147f2f1f2db4696f56323df5847f32692f
sam-butcher <samuel@typedb.com>
2024-12-12 16:31:35 +0000

    cleanup unused imports

--- module/connection/ServerDialog.kt
+++ module/connection/ServerDialog.kt
@@ -72,8 +72,6 @@ import com.typedb.studio.service.connection.DriverState.Status.CONNECTED
 import com.typedb.studio.service.connection.DriverState.Status.CONNECTING
 import com.typedb.studio.service.connection.DriverState.Status.DISCONNECTED
 import com.vaticle.typedb.driver.api.TypeDBCredential
-import java.net.URI
-import java.net.URISyntaxException
 import java.nio.file.Path
 import androidx.compose.ui.window.DialogState as ComposeDialogState
 
