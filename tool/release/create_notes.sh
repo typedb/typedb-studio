c416d5d6ecf13f7204004df77e06fb64cef728d8
Alex Walker <alex@typedb.com>
2025-04-02 12:23:38 +0100

    Delete basically everything

--- tool/release/create_notes.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-# This Source Code Form is subject to the terms of the Mozilla Public
-# License, v. 2.0. If a copy of the MPL was not distributed with this
-# file, You can obtain one at https://mozilla.org/MPL/2.0/.
-
- bazel run @typedb_dependencies//tool/release/notes:create -- typedb typedb-studio HEAD $(cat VERSION) ./RELEASE_TEMPLATE.md ./RELEASE_NOTES_LATEST.md
