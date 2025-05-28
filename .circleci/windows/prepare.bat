17eb250f2d5f3bd3734c6b9840af3f2dc959ed8a
Alex Walker <alex@typedb.com>
2025-05-28 16:53:49 +0100

    Windows CI: Rely on system-installed Node >:( and run tauri

--- .circleci/windows/prepare.bat
+++ .circleci/windows/prepare.bat
@@ -26,10 +26,12 @@ SETX BAZEL_PYTHON C:\Python311\python.exe
 SETX BAZEL_VC "C:\Program Files (x86)\Microsoft Visual Studio\2022\BuildTools\VC"
 
 REM install node modules
-nvm install 18
-nvm use 18
 npm install --global corepack@0.17.0
 corepack enable
 corepack prepare pnpm@8.6.7 --activate
 pnpm config set store-dir .pnpm-store
 pnpm install
+
+REM compile MSI installer using tauri
+pnpm build
+npx tauri build
