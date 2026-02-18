bee707e832279d8d518dbdac6ad2102c23ad7f11
Alex Walker <alex@typedb.com>
2026-02-18 21:02:49 +0000

    not an important change

--- src/module/about/app-info-dialog.component.ts
+++ src/module/about/app-info-dialog.component.ts
@@ -14,6 +14,7 @@ import { MatInputModule } from "@angular/material/input";
 import { ModalComponent, ModalCloseButtonComponent } from "../../framework/modal";
 import { FormActionsComponent, FormComponent, FormInputComponent } from "../../framework/form";
 import { ButtonComponent } from "../../framework/button/button.component";
+import { AnalyticsService } from "../../service/analytics.service";
 import { SnackbarService } from "../../service/snackbar.service";
 
 @Component({
@@ -23,10 +24,30 @@ import { SnackbarService } from "../../service/snackbar.service";
     imports: [FormsModule, ReactiveFormsModule, MatFormFieldModule, MatInputModule, FormActionsComponent]
 })
 export class AppInfoDialogComponent {
+    clickCount = 0;
+    easterEggState: "none" | "wobble" | "wobble-more" | "pre-explode" | "explode" = "none";
 
     constructor(
         private dialogRef: MatDialogRef<AppInfoDialogComponent>,
+        private analytics: AnalyticsService,
     ) {
+    }
+
+    onLogoClick() {
+        this.clickCount++;
+        if (this.easterEggState === "pre-explode" || this.easterEggState === "explode") {
+            return;
+        } else if (this.clickCount >= 30) {
+            this.easterEggState = "pre-explode";
+            setTimeout(() => {
+                this.easterEggState = "explode";
+                this.analytics.posthog.capture("typedb bot exploded");
+            }, 800);
+        } else if (this.clickCount >= 20) {
+            this.easterEggState = "wobble-more";
+        } else if (this.clickCount >= 10) {
+            this.easterEggState = "wobble";
+        }
     }
 
     close() {
