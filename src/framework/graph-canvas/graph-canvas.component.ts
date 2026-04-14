e42afd355a2f13f30a100bec14df957d08c81326
Alex Walker <alex@typedb.com>
2026-04-14 16:38:43 +0100

    Make graph vis controls responsive based on bg color of graph

--- src/framework/graph-canvas/graph-canvas.component.ts
+++ src/framework/graph-canvas/graph-canvas.component.ts
@@ -4,12 +4,14 @@
  * file, You can obtain one at https://mozilla.org/MPL/2.0/.
  */
 
-import { Component, ElementRef, EventEmitter, Input, Output, ViewChild } from "@angular/core";
+import { Component, ElementRef, EventEmitter, HostBinding, Input, OnDestroy, Output, ViewChild } from "@angular/core";
 import { MatTooltipModule } from "@angular/material/tooltip";
 import { ResizableDirective } from "@hhangular/resizable";
+import { Subscription } from "rxjs";
 import { GraphVisualiser } from "../graph-visualiser";
 import { GraphZoomControlsComponent } from "../graph-zoom-controls/graph-zoom-controls.component";
 import { GraphStylesPaneComponent } from "../graph-styles-pane/graph-styles-pane.component";
+import { GraphStyleService } from "../../service/graph-style.service";
 
 @Component({
     selector: "ts-graph-canvas",
@@ -17,7 +19,7 @@ import { GraphStylesPaneComponent } from "../graph-styles-pane/graph-styles-pane
     styleUrls: ["graph-canvas.component.scss"],
     imports: [MatTooltipModule, ResizableDirective, GraphZoomControlsComponent, GraphStylesPaneComponent],
 })
-export class GraphCanvasComponent {
+export class GraphCanvasComponent implements OnDestroy {
     @Input() visualiser: GraphVisualiser | null = null;
     @Input() queryRunning = false;
     @Input() graphPercent = 75;
@@ -29,6 +31,31 @@ export class GraphCanvasComponent {
     @Output() stylesPanePercentChange = new EventEmitter<number>();
 
     @ViewChild("canvasEl", { static: false }) canvasElRef?: ElementRef<HTMLElement>;
+
+    @HostBinding("class.graph-dark") isDark = true;
+    @HostBinding("class.graph-light") isLight = false;
+
+    private stylesSub: Subscription;
+
+    constructor(private styleService: GraphStyleService) {
+        this.stylesSub = this.styleService.styles$.subscribe(() => this.updateControlTheme());
+        this.updateControlTheme();
+    }
+
+    ngOnDestroy() {
+        this.stylesSub.unsubscribe();
+    }
+
+    private updateControlTheme() {
+        const hex = this.styleService.effectiveBackgroundHex;
+        const h = hex.startsWith("#") ? hex.slice(1) : hex;
+        const r = parseInt(h.substring(0, 2), 16);
+        const g = parseInt(h.substring(2, 4), 16);
+        const b = parseInt(h.substring(4, 6), 16);
+        const luminance = (0.299 * r + 0.587 * g + 0.114 * b) / 255;
+        this.isDark = luminance < 0.5;
+        this.isLight = luminance >= 0.5;
+    }
 
     get canvasEl(): HTMLElement | undefined {
         return this.canvasElRef?.nativeElement;
